beae81276348c9fce8f0d3fd68dece09e6c4cebd
jakub.kraus2 <jakub.kraus007@seznam.cz>
2023-03-15 22:33:54 +0100

    boss healthbar now shows boss name

--- HUD/boss/BossHealthBar.gd
+++ HUD/boss/BossHealthBar.gd
@@ -1,6 +1,9 @@
 extends CanvasLayer
 
 
+func _ready() -> void:
+	$BossName.text = get_parent().boss_name
+
 func _process(delta: float) -> void:
 	if PlayerBasicData.current_lives <= 0:
 		visible = false
