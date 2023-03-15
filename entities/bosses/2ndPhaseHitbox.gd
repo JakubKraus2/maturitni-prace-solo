40b487242b542bf881a6be732eb2d1340f3bb699
jakub.kraus2 <jakub.kraus007@seznam.cz>
2023-03-15 15:08:09 +0100

    HeartHorder 2nd phase hitbox position fix

--- entities/bosses/2ndPhaseHitbox.gd
+++ entities/bosses/2ndPhaseHitbox.gd
@@ -1,5 +1,5 @@
 extends CollisionShape2D
 
 
-func _process(delta):
-	global_position = Vector2(owner.get_parent().get_node("HeartHoarderBoss2").global_position.x, owner.get_parent().get_node("HeartHoarderBoss2").global_position.y + 33)
+func _physics_process(delta: float) -> void:
+	global_position = Vector2(owner.get_parent().get_node("HeartHoarderBoss2").global_position.x, owner.get_parent().get_node("HeartHoarderBoss2").global_position.y + 50)
