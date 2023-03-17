316f25410695462a22965276b4e943934c27643d
jakub.kraus2 <jakub.kraus007@seznam.cz>
2023-03-17 18:29:58 +0100

    Main Menu options button working, changed options menu -> no longer opens on Q, Escape Menu options button working

--- HUD/EscapeMenu/EscapeMenu.gd
+++ HUD/EscapeMenu/EscapeMenu.gd
@@ -1,5 +1,6 @@
 extends CanvasLayer
 
+onready var settingsmenu = load("res://ControlTest.tscn")
 
 
 func _input(event: InputEvent) -> void:
@@ -13,4 +14,8 @@ func _on_ContinueButton_pressed() -> void:
 	$AnimationPlayer.play("CLOSE")
 
 func _on_QuitToMenuButton_pressed() -> void:
-	get_tree().quit()
+	get_tree().change_scene("res://HUD/MainMenu/MainMenu.tscn")
+
+
+func _on_OptionsButton_pressed() -> void:
+	get_parent().add_child(settingsmenu.instance())
--- HUD/MainMenu/MainMenu.gd
+++ HUD/MainMenu/MainMenu.gd
@@ -1,6 +1,11 @@
 extends CanvasLayer
 
+onready var settingsmenu = load("res://ControlTest.tscn")
 
 
 func _on_QuitGameButton_pressed():
 	get_tree().quit()
+
+
+func _on_OptionsButton_pressed() -> void:
+	get_parent().add_child(settingsmenu.instance())
